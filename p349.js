d2e6135245a2f802ef939ede6827e8c29f66e665
Samin Amanat <amanat361@gmail.com>
2024-03-10 16:18:38 -0700

    added typescript support

--- p349.js
+++ p349.js
@@ -1,3 +1,13 @@
-// https://leetcode.com/problems/intersection-of-two-arrays/
-
-console.log("problem 349")
+function intersection(nums1, nums2) {
+    var seen = new Map();
+    for (var i = 0; i < nums1.length; i++) {
+        seen.set(nums1[i], false);
+    }
+    var bar = Array.from(new Set(nums2));
+    for (var i = 0; i < bar.length; i++) {
+        if (seen.has(bar[i])) {
+            seen.set(bar[i], true);
+        }
+    }
+    return [];
+}
---
+++ p349.ts
@@ -0,0 +1,13 @@
+function intersection(nums1: number[], nums2: number[]): number[] {
+  const seen = new Map();
+  for (let i = 0; i < nums1.length; i++) {
+    seen.set(nums1[i], false);
+  }
+  const bar = Array.from(new Set(nums2));
+  for (let i = 0; i < bar.length; i++) {
+    if (seen.has(bar[i])) {
+      seen.set(bar[i], true);
+    }
+  }
+  return []
+}
