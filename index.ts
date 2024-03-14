54310469f386f1cfb8e029eeb72bd0773e66ed5f
Samin Amanat <amanat361@gmail.com>
2024-03-13 20:40:28 -0700

    started working on problem 930

--- index.ts
+++ index.ts
@@ -1,5 +1,64 @@
-// https://leetcode.com/problems/trapping-rain-water/
+// https://leetcode.com/problems/binary-subarrays-with-sum/
 
-function trap(height: number[]): number {
-  return 0;  
-};
+//Input: nums = [1,0,1,0,1], goal = 2
+// Output: 4
+// Explanation: The 4 subarrays are bolded and underlined below:
+// [*1,0,1*,0,1]
+// [*1,0,1,0*,1]
+// [1,*0,1,0,1*]
+// [1,0,*1,0,1*]
+
+function numSubarraysWithSum(nums: number[], goal: number): number {
+  // initialize left and right pointers
+  let left = 0;
+  let right = 1;
+
+  // start with first value as current
+  let curr = nums[left];
+  let total_subarrays = 0;
+
+  function debugOutput() {
+    console.log("\n--------------------")
+    console.log(nums.slice(left,right))
+    console.log(`Left: ${left} ... Right: ${right}`)
+    console.log(`Curr: ${curr} ... Total Subarrays: ${total_subarrays}`)
+  }
+
+  function analyzeSubarray() {
+    if (curr === goal) {
+      total_subarrays += 1;
+    }
+  }
+
+  function traverseRight() {
+    while (curr <= goal && right < nums.length) {
+      curr += nums[right];
+      right += 1;
+      analyzeSubarray();
+    }
+  }
+
+  function traverseLeft() {
+    while (curr >= goal && left < right) {
+      curr -= nums[left];
+      left += 1;
+      analyzeSubarray();
+    }
+  }
+
+  while (right < nums.length) {
+    traverseRight()
+    debugOutput();
+
+    console.log("done moving right!!")
+
+    traverseLeft();
+    debugOutput()
+
+    console.log("done moving left!!")
+  }
+
+  return total_subarrays;
+}
+
+console.log(numSubarraysWithSum([1, 0, 1, 0, 1, 0, 0], 2));
