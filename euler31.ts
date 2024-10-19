c55f4d7c83d1ee3ba643182917d3c8a5bc8bcaa5
amanat361 <amanat361@gmail.com>
2024-10-19 16:50:20 -0700

    Refactor coin counting function in euler31.ts

--- euler31.ts
+++ euler31.ts
@@ -9,25 +9,45 @@ It is possible to make £2 in the following way:
 How many different ways can £2 be made using any number of coins?
 */
 
-const coins = [1, 2, 5, 10, 50, 100, 200]
+const base_coins = [10, 5, 1]
+const found = {} as Record<number,number>;
 
-function ideal_solution(amount: number, available_coins: number[]) {
-  let current = amount;
-  const used = {} as Record<number,number>;
-  coins.sort((a,b) => b - a).forEach(coin => {
-    while (coin <= current) {
-      current -= coin;
-      used[coin] ??= 0;
-      used[coin] += 1;
-    }
-  });
+function number_of_ways(current: number, coins: number[]): number {
 
-  return used
+  console.log("Currently looking at: " + current.toString());
+  console.log("Available coins are: " + coins);
+  console.log("\n")
+
+  // create a record of this number in our hash map
+  // if it exists, return its count, no need to recompute
+  if (!found[current]) {
+    found[current] = 0;
+  } else {
+    return found[current];
+  }
+
+  // select the biggest coin in the array (first one)
+  const biggest_coin = coins[0];
+
+  // if we are at one, there is only one way
+  // add one more way found and then return it
+  if (biggest_coin === 1) {
+    found[current] += 1;
+    return found[current];
+  }
+
+  // check if current is big enough for our coin
+  if (current > biggest_coin) {
+    // find number of ways minus that coin
+    found[current] += 1 + number_of_ways(current - biggest_coin, coins);
+    return found[current]
+  } else {
+    found[current] += 1 + number_of_ways(current, coins.slice(1,coins.length));
+    return found[current]
+  }
+
 }
 
-function how_many_ways(amount: number): number {
-  
-
-  return 0;
-}
-
+const answer = number_of_ways(23, base_coins)
+console.log(answer)
+console.log(found)
